f9f676277a8e4e0ea2a761553a65a3efa8f406ea
Alexandre <alexandrehrtm@protonmail.com>
2020-08-11 19:05:28 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+import Chart from './components/Chart'
+
+
+function App() {
+  return (
+    <>
+      <Chart />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App;
